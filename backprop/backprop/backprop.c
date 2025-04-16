6befa377d2fa7e5809f8208d5daf3173dddec1b8
Michele Fiorito <michele.fiorito@polimi.it>
2025-04-16 23:17:43 +0200

    Fix backprop type parametrization

--- backprop/backprop/backprop.c
+++ backprop/backprop/backprop.c
@@ -16,8 +16,8 @@ void soft_max(TYPE net_outputs[possible_outputs], TYPE activations[possible_outp
 void RELU(TYPE activations[nodes_per_layer], TYPE dactivations[nodes_per_layer], int size) {
     int i;
     for( i = 0; i < size; i++) {
-        dactivations[i] = activations[i]*(1.0-activations[i]);
-        activations[i] = 1.0/(1.0+exp(-activations[i]));
+        dactivations[i] = activations[i]*((TYPE)1.0-activations[i]);
+        activations[i] = (TYPE)1.0/((TYPE)1.0+exp(-activations[i]));
     }
 }
 
@@ -156,18 +156,18 @@ void update_weights(TYPE weights1[input_dimension*nodes_per_layer],
                     TYPE d_biases2[nodes_per_layer],
                     TYPE d_biases3[possible_outputs]) {
     int i, j;
-    double norm, bias_norm;
-    norm = 0.0;
-    bias_norm = 0.0;
+    TYPE norm, bias_norm;
+    norm = (TYPE)0.0;
+    bias_norm = (TYPE)0.0;
 
     for(i=0; i < input_dimension; i++){
         for(j = 0; j < nodes_per_layer; j++){
-            weights1[i*nodes_per_layer + j] -= (d_weights1[i*nodes_per_layer + j] * learning_rate);
+            weights1[i*nodes_per_layer + j] -= (d_weights1[i*nodes_per_layer + j] * (TYPE)learning_rate);
             norm += weights1[i*nodes_per_layer + j]*weights1[i*nodes_per_layer + j];
         }
     }
     for(i=0; i < nodes_per_layer; i++){
-        biases1[i] -= (d_biases1[i]*learning_rate);
+        biases1[i] -= (d_biases1[i]*(TYPE)learning_rate);
         bias_norm += biases1[i]*biases1[i];
     }
     
@@ -183,17 +183,17 @@ void update_weights(TYPE weights1[input_dimension*nodes_per_layer],
         biases1[i] = (biases1[i]/bias_norm);
     }
 
-    norm = (double)0.0;
-    bias_norm = (double)0.0;
+    norm = (TYPE)0.0;
+    bias_norm = (TYPE)0.0;
 
     for(i=0; i < nodes_per_layer; i++){
         for(j = 0; j < nodes_per_layer; j++){
-            weights2[i*nodes_per_layer + j] -= (d_weights2[i*nodes_per_layer + j] * learning_rate);
+            weights2[i*nodes_per_layer + j] -= (d_weights2[i*nodes_per_layer + j] * (TYPE)learning_rate);
             norm += weights2[i*nodes_per_layer + j]*weights2[i*nodes_per_layer + j];
         }
     }
     for(i=0; i < nodes_per_layer; i++){
-        biases2[i] -= (d_biases2[i]*learning_rate);
+        biases2[i] -= (d_biases2[i]*(TYPE)learning_rate);
         bias_norm += biases2[i]*biases2[i];
     }
 
@@ -209,17 +209,17 @@ void update_weights(TYPE weights1[input_dimension*nodes_per_layer],
         biases2[i] = (biases2[i]/bias_norm);
     }
 
-    norm = (double)0.0;
-    bias_norm = (double)0.0;
+    norm = (TYPE)0.0;
+    bias_norm = (TYPE)0.0;
 
     for(i=0; i < nodes_per_layer; i++){
         for(j = 0; j < possible_outputs; j++){
-            weights3[i*possible_outputs + j] -= (d_weights3[i*possible_outputs + j] * learning_rate);
+            weights3[i*possible_outputs + j] -= (d_weights3[i*possible_outputs + j] * (TYPE)learning_rate);
             norm += weights3[i*possible_outputs + j]*weights3[i*possible_outputs + j];
         }
     }
     for(i=0; i<possible_outputs;i++){
-        biases3[i] -= d_biases3[i]*learning_rate;
+        biases3[i] -= d_biases3[i]*(TYPE)learning_rate;
         bias_norm += biases3[i]*biases3[i];
     }
 
